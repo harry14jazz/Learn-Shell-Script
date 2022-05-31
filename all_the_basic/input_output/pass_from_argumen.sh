73febc97da8f20d4c05eb0b66f1fbea00cb19900
harry14jazz <harry14jazz@gmail.com>
2022-05-31 21:57:26 +0700

    Input output

---
+++ all_the_basic/input_output/pass_from_argumen.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "This words below is get from argument/outside while running this script"
+echo "$1"
+echo "$2"
---
+++ all_the_basic/input_output/practice_input_output.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Input your bio below!"
+read -p "Enter your name: " my_name
+read -p "Enter your age: " my_age
+
+echo "So, your name is $my_name and in uppercase is ${my_name^^}"
+echo "And your age is $my_age"
